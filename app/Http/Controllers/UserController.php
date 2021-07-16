9dcc8ce00418cda4ef234047029c25911c72985d
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2021-07-16 07:34:12 +0200

    Repair password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = $request->password;
+        $user->password = Hash::make('password');
         $user->save();
         return $user;
     }
